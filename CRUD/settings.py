5bcaa53eaee388d9448306599f6c423caec93300
lukasredfield <lukasredfield@gmail.com>
2022-11-11 02:32:41 -0300

    save 24 configuración RENDER

--- CRUD/settings.py
+++ CRUD/settings.py
@@ -135,11 +135,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
 
-if not DEBUG:    #Si no esta en desarrollo el proyecto va a utilizar los archivos estaticos de esta otra ruta:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+    os.path.join(BASE_DIR, "static")
+]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 
