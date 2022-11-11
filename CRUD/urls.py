01c71d01c6d71ee6958ad8a3a91ad636d7fd4d83
lukasredfield <lukasredfield@gmail.com>
2022-11-11 00:55:25 -0300

    save 09 configuración RENDER

--- CRUD/urls.py
+++ CRUD/urls.py
@@ -13,12 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from projects import views
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/', admin.site.urls),
+    path('home/', views.home, name="Home"),
     path('', include('projects.urls')),
 
 ]
